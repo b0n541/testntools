74c726bf0762f8969d1efe1dde340d2404f925dd
b0n541 <b0n541@github>
2012-09-19 14:22:05 +0200

    First java class

---
+++ testntools/src/main/java/net/b0n541/Blubb.java
@@ -0,0 +1,12 @@
+package net.b0n541;
+
+public class Blubb
+{
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Blubb");
+    }
+}
